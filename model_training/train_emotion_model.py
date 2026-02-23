e8bfce7ae4e686d0a1977328cb2fc01368917725
Yash Lade <yash.lade@softude.com>
2026-02-23 17:25:03 +0530

    added model training module

---
+++ model_training/train_emotion_model.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+import joblib
+
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
+from sklearn.svm import LinearSVC
+
+# -----------------------------
+# 1. Load Dataset
+# -----------------------------
+data = pd.read_csv("data/emotion_dataset_final.csv")
+
+X = data["text"]
+y = data["emotion"]
+
+# -----------------------------
+# 2. Train-Test Split
+# -----------------------------
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
+
+# -----------------------------
+# 3. TF-IDF Vectorization
+# -----------------------------
+vectorizer = TfidfVectorizer(
+    max_features=5000,
+    ngram_range=(1,2)
+)
+
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
+
+# -----------------------------
+# 4. Support Vector Machine
+# -----------------------------
+model = LinearSVC(class_weight="balanced")
+model.fit(X_train_tfidf, y_train)
+
+# -----------------------------
+# 5. Evaluation
+# -----------------------------
+y_pred = model.predict(X_test_tfidf)
+
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nClassification Report:\n")
+print(classification_report(y_test, y_pred))
+
+print("\nConfusion Matrix:\n")
+print(confusion_matrix(y_test, y_pred))
+
+# -----------------------------
+# 6. Save Model + Vectorizer
+# -----------------------------
+joblib.dump(model, "models/emotion_model_svm.pkl")
+joblib.dump(vectorizer, "models/tfidf_vectorizer_svm.pkl")
+
+print("\nModel and vectorizer saved successfully.")
